6da2dd46370173342fbd8cd8bbb06607d7d76e8d
João Tambue <tjoao7279@gmail.com>
2026-01-13 23:21:20 +0100

    Update API_BASE_URL to new development tunnel

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_ACTIVITY_ID = 1;
-export const API_BASE_URL = "https://voting-system-backend-zw80.onrender.com";
+export const API_BASE_URL = "https://kmgtz1w8-8000.uks1.devtunnels.ms/";
+// export const API_BASE_URL = "https://voting-system-backend-zw80.onrender.com";
 export const STORAGE_CRYPTO_KEY = "YrRrapp0P6wdwJpwXRfUI6LM0E9TxCkO";
